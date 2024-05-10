8a3064eb76856c7701d5092ec0349830ddeb0bde
KetkiAshtankar <ketkiashtankar29@gmail.com>
2024-05-11 02:16:26 +0530

    Added some content

---
+++ test_demo1.py
@@ -0,0 +1,9 @@
+import pytest
+
+@pytest.mark.smoke
+def test_firstprogram(setup):
+    print("Hi, This is my first Line of code in Pytest")
+
+#
+# def test_thirdprogram_credit():
+#     print("Hi, This is my first Line of code in Pytest (credit)")
